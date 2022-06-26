d1617c88a9cebe86bab59af965d8c6f0206d86dd
kesaavan <kesaavan007@gmail.com>
2022-06-26 15:40:46 +0530

    Validations

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,2 +1,5 @@
 class Laptop < ApplicationRecord
+	validates :name, :model, :configuration, presence:true 
+	validates :name, length: {minimum:2 , maximum:50}
+	validates :configuration, length: {minimum:10, maximum:1000}
 end
